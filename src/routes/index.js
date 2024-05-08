1de7fd88cf78a98504c357316494b6419c7ba24b
Enzo Gabriel Guedes <eggqsaraiva@gmail.com>
2024-05-07 21:18:42 -0300

    add vaccination, vaccine and disease routes to default routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,15 @@
 import userRoutes from "./user.routes.js";
 import authRoutes from "./auth.routes.js";
+import diseaseRoutes from "./disease.routes.js";
+import vaccineRoutes from "./vaccine.routes.js";
+import vaccinationRoutes from "./vaccination.routes.js";
 
 const routes = app => {
   userRoutes(app);
   authRoutes(app);
+  diseaseRoutes(app);
+  vaccineRoutes(app);
+  vaccinationRoutes(app);
 };
 
 export default routes;
